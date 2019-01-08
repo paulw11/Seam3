2d760210d26d051b7108ad9f223fdcea300cd9a8
Paul Wilkinson <paulw@wilko.me>
2019-01-09 07:53:30 +1100

    Update for Seam3 1.5.0

--- Example/Seam3_Example_TVOS/AppDelegate.swift
+++ Example/Seam3_Example_TVOS/AppDelegate.swift
@@ -221,8 +221,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         print("Received push")
-        self.smStore?.handlePush(userInfo: userInfo)
-        completionHandler(.newData)
+        self.smStore?.handlePush(userInfo: userInfo) { (result) in
+            completionHandler(result.uiBackgroundFetchResult)
+        }
+       
     }
     
 }
